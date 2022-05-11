d568fc79a7d3b637128fd15c42d9644fdd20b294
Isaac Wasserman <isaacrwasserman@gmail.com>
2022-05-11 15:35:37 -0400

    reorganization

--- inflection/test_augmentation.py
+++ inflection/test_augmentation.py
@@ -17,7 +17,7 @@ progress_bar = "[" + "".join([" "] * max_progress) + "]"
 for language in languages:
     print(f'Working on {language}, regular')
     os.system(f'mv sigmorphon_data/{language}-hall-regular sigmorphon_data/{language}-hall')
-    os.system(f'python inflection.py --datapath sigmorphon_data --L2 {language} --mode train --use_hall > train-{language}-regular.log 2>&1')
+    os.system(f'python inflection.py --datapath sigmorphon_data --L2 {language} --mode train --use_hall > train_logs/train-{language}-regular.log 2>&1')
     os.system(f'mv sigmorphon_data/{language}-hall sigmorphon_data/{language}-hall-regular')
     os.system(f'mv models/{language} models/{language}-hall-regular')
     progress += 1
@@ -26,7 +26,7 @@ for language in languages:
 
     print(f'Working on {language}, ngram')
     os.system(f'mv sigmorphon_data/{language}-hall-ngram sigmorphon_data/{language}-hall')
-    os.system(f'python inflection.py --datapath sigmorphon_data --L2 {language} --mode train --use_hall > train-{language}-ngram.log  2>&1')
+    os.system(f'python inflection.py --datapath sigmorphon_data --L2 {language} --mode train --use_hall > train_logs/train-{language}-ngram.log  2>&1')
     os.system(f'mv sigmorphon_data/{language}-hall sigmorphon_data/{language}-hall-ngram')
     os.system(f'mv models/{language} models/{language}-hall-ngram')
     progress += 1
@@ -35,7 +35,7 @@ for language in languages:
 
     print(f'Working on {language}, smart')
     os.system(f'mv sigmorphon_data/{language}-hall-smart sigmorphon_data/{language}-hall')
-    os.system(f'python inflection.py --datapath sigmorphon_data --L2 {language} --mode train --use_hall > train-{language}-smart.log  2>&1')
+    os.system(f'python inflection.py --datapath sigmorphon_data --L2 {language} --mode train --use_hall > train_logs/train-{language}-smart.log  2>&1')
     os.system(f'mv sigmorphon_data/{language}-hall sigmorphon_data/{language}-hall-smart')
     os.system(f'mv models/{language} models/{language}-hall-smart')
     progress += 1
